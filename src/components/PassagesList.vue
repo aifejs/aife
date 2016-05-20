5fca182a684bf7b4f509b328edc98d4979b20f11
Konstantin Kitmanov <doctor.hogart@gmail.com>
2016-05-21 00:33:15 +0200

    Add quick filtering for passages, close #2

--- src/components/PassagesList.vue
+++ src/components/PassagesList.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
     article.passagesList
         sorter-buttons("v-bind:sort-properties"="sortProperties", "@sort"="setPassagesSorting")
+        input(type="search", "v-bind:value"="getPassagesFiltering", "@input"="setPassagesFiltering")
         ul.unstyled
             li(v-for="passage of passagesOverview")
                 // we have to use @click to create a tab and we have to use v-link for routes to work
-                a(v-link="{name: 'passage', params: {pid: passage.pid}}", "@click"="openPassage(passage.pid)") {{passage.title}}
+                a(v-link="{name: 'passage', params: {pid: passage.pid}}", "@click"="openPassage(passage.pid)", v-hilite-term="getPassagesFiltering") {{passage.title}}
                 a.btn.btn-xs.btn-danger(v-link="{name: 'deletePassage', params: {pid: passage.pid}}")
                     span.glyphicon.glyphicon-ban-circle
-                p {{passage.text}}
+                p(v-hilite-term="getPassagesFiltering") {{passage.text}}
 </template>
 
 <style>
@@ -18,9 +19,10 @@
 </style>
 
 <script>
-    import {openPassage,setPassagesSorting,} from '../vuex/actions';
-    import {passagesOverview,} from '../vuex/getters';
+    import {openPassage,setPassagesSorting,setPassagesFiltering,} from '../vuex/actions';
+    import {passagesOverview,getPassagesFiltering} from '../vuex/getters';
     import SorterButtons from './SorterButtons.vue';
+    import HiliteTerm from '../directives/HiliteTerm';
 
     export default {
         name: 'passages-list',
@@ -37,14 +39,20 @@
         vuex: {
             getters: {
                 passagesOverview,
+                getPassagesFiltering,
             },
             actions: {
                 setPassagesSorting,
+                setPassagesFiltering,
             },
         },
 
         components: {
             SorterButtons,
+        },
+
+        directives: {
+            HiliteTerm,
         },
     };
 </script>
