36370ccb4e7d1c87d7afbd707d05d59fb4aeb8ed
Konstantin Kitmanov <doctor.hogart@gmail.com>
2016-05-21 22:30:59 +0200

    Basic reusable wrapper for CodeMirror, see #1

---
+++ src/components/CodeMirror.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+    div.codeEditor
+</template>
+
+<script>
+    import CodeMirror from 'codemirror';
+
+    /**
+     * Generic reusable code editor component, adapted from Twine source
+     */
+    export default {
+        name: 'code-mirror',
+        props: {
+            options: Object,
+            code: String,
+        },
+
+        watch: {
+            code() {
+                if (this.code !== this.codeMirror.getValue()) {
+                    this.codeMirror.setValue(this.code);
+                }
+            },
+        },
+
+        compiled() {
+            this.codeMirror = new CodeMirror(this.$el, this.options);
+            this.codeMirror.setValue(this.code);
+
+            this.codeMirror.on('change', this.onCodeMirrorChange.bind(this));
+
+            // this is some hack, without it CodeMirror renders blank
+            setTimeout(this.codeMirror.refresh.bind(this.codeMirror), 100);
+        },
+
+        attached() {
+            this.codeMirror.focus();
+        },
+
+        methods: {
+            onCodeMirrorChange() {
+                this.code = this.codeMirror.getValue();
+
+                // note kebab-case here, event names are not automatically converted
+                this.$emit('code-changed', this.code);
+            },
+        },
+    };
+</script>
