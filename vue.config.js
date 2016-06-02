9af5a39dbe170bdb7694becace06dd19959e90c3
Konstantin Kitmanov <doctor.hogart@gmail.com>
2016-06-02 21:52:11 +0200

    add reasonable autoprefixer config

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,8 @@ module.exports = {
         'include css': true,
         'resolve url': true,
     },
+
+    autoprefixer: {
+        browsers: ['last 2 Chrome versions', 'last 2 Firefox versions', 'ChromeAndroid > 50', 'Edge >= 14',],
+    },
 };
