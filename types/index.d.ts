2ed322b8bca1b26cd89b5084af489bb5744e742e
Konstantin Kitmanov <doctor.hogart@gmail.com>
2016-11-01 18:35:21 +0100

    add some typescript definitions

---
+++ types/index.d.ts
@@ -0,0 +1,116 @@
+type Dictionary<T> = { [key: string]: T };
+
+interface IStory {
+    title: string,
+    ifid: string,
+    passages: IPassage[],
+    opened: IPassage[],
+    styleSheet: string,
+    editStylesheet: boolean,
+    script: string,
+    editScript: boolean,
+    customHtml: string,
+    editCustomHtml: boolean,
+    editProof: boolean,
+    lastEdit: Date | number,
+    format: string,
+}
+
+interface IPassage {
+    title: string,
+    text: string,
+    pid: number,
+    tags: string[],
+    starting: boolean,
+}
+
+interface ITwineStory {
+    startPassagePid: number,
+    name: string,
+    ifid: string,
+    lastUpdate: Date,
+    script: string,
+    stylesheet: string,
+    zoom: number,
+    passages: ITwinePassage[],
+}
+
+interface ITwinePassage {
+    pid: string,
+    left: number,
+    top: number,
+    width: number,
+    height: number,
+    tags: string[],
+    name: string,
+    text: string,
+}
+
+
+interface ISorting {
+    field: string,
+    sort: 'asc' | 'desc',
+}
+
+interface IEditorOptions {
+    lineWrapping: boolean,
+    lineNumbers: boolean,
+    indentUnit: number,
+    gutters: string[],
+    lint: boolean,
+    mode?: string,
+}
+
+import VueRouter from 'vue-router'
+
+interface IRoute {
+    params?: {
+        ifid?: string,
+        pid?: number
+        passageTitle?: string,
+    }
+}
+
+interface IState {
+    route: VueRouter.Route & IRoute,
+
+    stories: IStory[],
+
+    passagesSorting: ISorting,
+    storiesSorting: ISorting,
+    passagesFiltering: string,
+
+    codeEditorOptions: IEditorOptions,
+
+    storiesLoaded: boolean,
+
+    proofModeError: boolean,
+}
+
+interface IFormatProperties {
+    setup?: () => void;
+    source: string,
+}
+
+interface IPublishOptions {
+    formatOptions?: string[],
+    startPassageId?: number,
+}
+
+interface ITextStats {
+    words: number,
+    characters: number,
+    charactersAll: number,
+}
+
+interface IStoryStats extends ITextStats {
+    passages: number,
+    code: number,
+}
+
+interface IStoryItem {
+    title: string,
+    stats: IStoryStats,
+    ifid: string,
+    passages: IPassage[],
+}
