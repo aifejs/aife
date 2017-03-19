43ece0871feaebbdc256d837e843c968129799f5
Konstantin Kitmanov <doctor.hogart@gmail.com>
2017-03-19 17:22:39 +0100

    add Rect class, improve Vector2 jsdocs

---
+++ src/lib/Rect.js
@@ -0,0 +1,88 @@
+import {Vector2,} from './Vector2';
+
+export class Rect {
+    get bottomLeft() {
+        return this.start;
+    }
+
+    get topLeft() {
+        return new Vector2(this.start.x, this.end.y);
+    }
+
+    get topRight() {
+        return this.end;
+    }
+
+    get bottomRight() {
+        return new Vector2(this.end.x, this.start.y);
+    }
+
+    /**
+     * @param {Vector2} start
+     * @param {Vector2}end
+     */
+    constructor(start, end) {
+        const [normStart, normEnd,] = this.normalize(start, end);
+
+        /** @type Vector2 */
+        this.start = normStart;
+        /** @type Vector2 */
+        this.end = normEnd;
+        /** @type Vector2 */
+        this.center = this.start.clone().add(this.end).divide(2);
+    }
+
+    /**
+     * @param {Vector2} start
+     * @param {Vector2}end
+     * @return {Vector2[]}
+     */
+    normalize(start, end) {
+        if (start.gt(end)) {
+            return [end, start,];
+        } else {
+            return [start, end,];
+        }
+    }
+    /**
+     * @param {Vector2} point
+     * @return {boolean}
+     */
+    containsPoint(point) {
+        return this.start.lte(point) && this.end.gte(point);
+    }
+
+    /**
+     * @param {Rect} rect
+     * @return {boolean}
+     */
+    intersectsRect(rect) {
+        if (this.containsPoint(rect.center)) {
+            return true;
+        } else {
+            const points = ['bottomLeft', 'topLeft', 'topRight', 'bottomRight',];
+            let intersects = false;
+            for (let i = 0, len = points.length; i < len; i++) {
+                if (this.containsPoint(rect[points[i]])) {
+                    intersects = true;
+                    break;
+                }
+            }
+
+            return intersects;
+        }
+    }
+
+    /**
+     *
+     * @param {Vector2} start
+     * @param {Vector2} size
+     * @return {Rect}
+     */
+    static fromStartAndSize(start, size) {
+        return new Rect(
+            start,
+            start.clone().add(size)
+        );
+    }
+}
--- src/lib/Vector2.js
+++ src/lib/Vector2.js
@@ -3,11 +3,19 @@ export class Vector2 {
         return Math.hypot(this.x, this.y);
     }
 
+    /**
+     * @param {number} x
+     * @param {number} y
+     */
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {Vector2}
+     */
     add(other) {
         this.x += other.x;
         this.y += other.y;
@@ -15,6 +23,10 @@ export class Vector2 {
         return this;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {Vector2}
+     */
     subtract(other) {
         this.x -= other.x;
         this.y -= other.y;
@@ -22,6 +34,10 @@ export class Vector2 {
         return this;
     }
 
+    /**
+     * @param {number} amount
+     * @return {Vector2}
+     */
     multiply(amount) {
         this.x *= amount;
         this.y *= amount;
@@ -29,6 +45,10 @@ export class Vector2 {
         return this;
     }
 
+    /**
+     * @param {number} amount
+     * @return {Vector2}
+     */
     divide(amount) {
         this.x /= amount;
         this.y /= amount;
@@ -36,18 +56,32 @@ export class Vector2 {
         return this;
     }
 
+    /**
+     * @return {Vector2}
+     */
     unit() {
         return this.divide(this.length);
     }
 
+    /**
+     * @return {Vector2}
+     */
     reverse() {
         return this.multiply(-1);
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {number}
+     */
     dot(other) {
         return this.x * other.x + this.y * other.y;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {number}
+     */
     angleBetween(other) {
         const dot = this.dot(other);
         const angleCos = dot / (this.length * other.length);
@@ -55,26 +89,49 @@ export class Vector2 {
         return Math.acos(angleCos);
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {boolean}
+     */
     lt(other) {
         return this.length < other.length;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {boolean}
+     */
     lte(other) {
         return this.length <= other.length;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {boolean}
+     */
     gt(other) {
         return this.length > other.length;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {boolean}
+     */
     gte(other) {
         return this.length > other.length;
     }
 
+    /**
+     * @param {Vector2} other
+     * @return {boolean}
+     */
     eq(other) {
         return this.x === other.x && this.y === other.y;
     }
 
+    /**
+     * @return {Vector2}
+     */
     clone() {
         return new Vector2(this.x, this.y);
     }
@@ -83,26 +140,47 @@ export class Vector2 {
         return `(${this.x},${this.y})`;
     }
 
+    /**
+     * @return {Vector2}
+     */
     static zero() {
         return new Vector2(0, 0);
     }
 
+    /**
+     * @return {Vector2}
+     */
     static up() {
         return new Vector2(0, 1);
     }
 
+    /**
+     * @return {Vector2}
+     */
     static right() {
         return new Vector2(1, 0);
     }
 
+    /**
+     * @return {Vector2}
+     */
     static down() {
         return new Vector2(0, -1);
     }
 
+    /**
+     * @return {Vector2}
+     */
     static left() {
         return new Vector2(-1, 0);
     }
 
+    /**
+     * @param {DOMRect} domRect
+     * @param xName
+     * @param yName
+     * @return {Vector2}
+     */
     static fromDOMRect(domRect, xName = 'left', yName = 'top') {
         return new Vector2(domRect[xName], domRect[yName]);
     }
